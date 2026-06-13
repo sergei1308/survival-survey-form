21da9fca8c2622a846145cdfe5a39770b0664ab5
sergei1308 <67683568+sergei1308@users.noreply.github.com>
2026-06-13 13:26:33 +0200

    Connect participant form to Supabase submission API

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SURVEY_CONFIG = {
-  submitUrl: "",
-  turnstileSiteKey: ""
+  submitUrl: "https://buibymhyvrokvrnocygg.supabase.co/functions/v1/submit-response",
+  turnstileSiteKey: "0x4AAAAAADkBoQZx9RquGycv"
 };
